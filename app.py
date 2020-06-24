e92f5edc3ca329887e910398ed7b0d33aca5a762
Shivansh Singhal <shivanshsinghal107@gmail.com>
2020-06-24 20:22:53 +0530

    Added functions to display user portfolio & list of assets

--- app.py
+++ app.py
@@ -1,3 +1,8 @@
+import os
+import datetime
+import yfinance as yf
+from pandas_datareader import data as pdr
+
 from flask import Flask, session, request, render_template, redirect
 from flask_session import Session
 from sqlalchemy import create_engine
@@ -14,6 +19,8 @@ Session(app)
 engine = create_engine(os.getenv("DATABASE_URL", "sqlite:///database.db"))
 db = scoped_session(sessionmaker(bind=engine))
 
+yf.pdr_override()
+
 @app.route("/", methods = ['GET', 'POST'])
 def index():
     if request.method == 'GET':
@@ -22,8 +29,8 @@ def index():
         else:
             return render_template("index.html", loginstatus = "False")
     else:
-        asset = request.form.get("asset").lower().replace(" ", "+")
-        return redirect(f"127.0.0.1:5000/{asset}")
+        category = request.form.get("asset").lower().replace(" ", "+")
+        return redirect(f"/{category}")
 
 @app.route("/register", methods = ['GET', 'POST'])
 def register():
@@ -77,11 +84,81 @@ def logout():
     print(session.keys())
     return redirect("http://127.0.0.1:5000")
 
-@app.route("/<asset>", methods = ['GET', 'POST'])
-def get_assets(asset):
-    a = asset.replace("+", " ")
-    data = db.execute("SELECT * FROM assets WHERE type = :type", {"type": a}).fetchall()
+@app.route("/<category>", methods = ['GET', 'POST'])
+def get_assets(category):
+    c = category.replace("+", " ")
+    data = db.execute("SELECT * FROM assets WHERE type = :type", {"type": c}).fetchall()
+    prices = []
+    for d in data:
+        price = pdr.get_data_yahoo(d.yf_symbol, start = str(datetime.datetime.utcnow())[:10])
+        if len(price) > 1:
+            prices.append(round(price.Close[1], 2))
+        elif len(price) > 0:
+            prices.append(round(price.Close[0], 2))
+        else:
+            prices.append("NA")
+    db.close()
     if session.get("logged_in"):
-        return render_template("asset.html", loginstatus = "True", curruser = session["username"], asset = a.title(), table = data)
+        return render_template("asset.html", loginstatus = "True", curruser = session["username"], category = c.title(), data = data, prices = prices)
     else:
-        return render_template("asset.html", loginstatus = "False", asset = a.title(), table = data)
+        return render_template("asset.html", loginstatus = "False", category = c.title(), data = data, prices = prices)
+
+@app.route("/buy/<id>/<price>", methods = ['GET', 'POST'])
+def buy(id, price):
+    if request.method == 'POST':
+        quantity = int(request.form.get("quantity"))
+        username = session["username"]
+        d = str(datetime.datetime.utcnow())[:10].split("-")
+        date = f"{d[2]}-{d[1]}-{d[0]}"
+        asset = db.execute("SELECT * FROM assets WHERE id = :id", {"id": id}).fetchall()[0]
+        db.execute("INSERT INTO investment (username, asset, curr_price, quantity, date) VALUES (:username, :asset, :curr_price, :quantity, :date)", {"username": username, "asset": asset.name, "curr_price": price, "quantity": quantity, "date": date})
+        print(f"Invested in {asset.name}")
+        db.commit()
+        db.close()
+        return "<script>alert('Investment Successful'); window.location = window.history.back();</script>"
+    else:
+        return "<script>alert('Method not allowed'); window.location = window.history.back();</script>"
+
+@app.route("/portfolio", methods = ['GET', 'POST'])
+def portfolio():
+    if session.get("logged_in"):
+        invs = db.execute("SELECT * FROM investment WHERE username = :username ORDER BY date DESC", {"username": session["username"]}).fetchall()
+        print(len(invs))
+        symbols = []
+        prices = []
+        currency = []
+        type = []
+        change = []
+        pchange = []
+        net_pl = 0
+        total = 0
+        for i in invs:
+            d = db.execute("SELECT * FROM assets WHERE name = :name", {"name": i.asset}).fetchall()[0]
+            price = pdr.get_data_yahoo(d.yf_symbol, start = str(datetime.datetime.utcnow())[:10])
+            if len(price) > 1:
+                p = round(price.Close[1], 2)
+                prices.append(p)
+                change.append(round(p - i.curr_price, 2))
+                pchange.append(round((1 - i.curr_price/p), 4))
+                net_pl += (p - i.curr_price) * i.quantity
+            elif len(price) > 0:
+                p = round(price.Close[0], 2)
+                prices.append(p)
+                change.append(round(p - i.curr_price, 2))
+                pchange.append(round((1 - i.curr_price/p), 4))
+                net_pl += (p - i.curr_price) * i.quantity
+            else:
+                prices.append("NA")
+                change.append("NA")
+                pchange.append("NA")
+            symbols.append(d.symbol)
+            currency.append(d.currency)
+            type.append(d.type)
+            total += i.curr_price * i.quantity
+        db.close()
+        return render_template("portfolio.html", curruser = session["username"], invs = invs, symbols = symbols, prices = prices, currency = currency, type = type, change = change, pchange = pchange, net_pl = round(net_pl, 2), total = total)
+    else:
+        return "<script>alert('Login first'); window.location = 'http://127.0.0.1:5000/login';</script>"
+
+#@app.route("/<category>/<asset>", methods = ['GET', 'POST'])
+#def display_asset(category, asset):
