43e3b0c62e01d6ed57d952f0e4ed800555ab6f4b
Shivansh Singhal <shivanshsinghal107@gmail.com>
2020-06-24 20:21:39 +0530

    Initialized & structured the database

--- import.py
+++ import.py
@@ -1,3 +1,6 @@
+import os
+import csv
+
 from flask import Flask, session
 from flask_session import Session
 from sqlalchemy import create_engine
@@ -16,8 +19,43 @@ db = scoped_session(sessionmaker(bind=engine))
 
 db.execute('''CREATE TABLE IF NOT EXISTS users (username VARCHAR(16) NOT NULL, password VARCHAR(64) NOT NULL, join_date text,
             PRIMARY KEY(username))''')
-db.execute('''CREATE TABLE IF NOT EXISTS email (mail VARCHAR(64) NOT NULL, username VARCHAR(16) NOT NULL,
-            FOREIGN KEY(username) REFERENCES users(username) ON DELETE CASCADE ON UPDATE CASCADE,
-            PRIMARY KEY(mail, username))''')
+db.execute('''CREATE TABLE IF NOT EXISTS email (mail VARCHAR(64) NOT NULL, username VARCHAR(16) NOT NULL, FOREIGN KEY(username)
+            REFERENCES users(username) ON DELETE CASCADE ON UPDATE CASCADE, PRIMARY KEY(mail, username))''')
 db.execute('''CREATE TABLE IF NOT EXISTS assets (id INTEGER PRIMARY KEY AUTOINCREMENT, type VARCHAR(40), name VARCHAR(50),
-            symbol VARCHAR(10))''')
+            currency VARCHAR(5), symbol VARCHAR(10), yf_symbol VARCHAR(15), yf_name VARCHAR(50), url VARCHAR(100))''')
+db.execute('''CREATE TABLE IF NOT EXISTS investment (id INTEGER PRIMARY KEY AUTOINCREMENT, username VARCHAR(16) NOT NULL,
+            asset VARCHAR(50), curr_price FLOAT, quantity INTEGER, date text, FOREIGN KEY(username) REFERENCES users(username) ON DELETE CASCADE ON UPDATE CASCADE, FOREIGN KEY(asset) REFERENCES assets(name) ON DELETE CASCADE ON UPDATE CASCADE)''')
+db.commit()
+
+def insert_assets():
+    print("Inserting Commodities in the assets table")
+    type = "commodities"
+    f = open("commodities.csv")
+    reader = csv.reader(f)
+    count = 0
+    for symbol, name, url, yf_symbol, currency, yf_name in reader:
+        if count == 0:
+            count += 1
+        else:
+            db.execute("INSERT INTO assets (type, name, currency, symbol, yf_symbol, yf_name, url) VALUES (:type, :name, :currency, :symbol, :yf_symbol, :yf_name, :url)", {"type": type, "name": name, "currency": currency, "symbol": symbol, "yf_symbol": yf_symbol, "yf_name": yf_name, "url": url})
+            print(f"{count}. {name} Inserted")
+            count += 1
+
+    print()
+
+    print("Inserting Forex Currencies in the assets table")
+    type = "forex currencies"
+    f = open("forex_currencies.csv")
+    reader = csv.reader(f)
+    count = 0
+    for symbol, name, url, yf_symbol, currency, yf_name in reader:
+        if count == 0:
+            count += 1
+        else:
+            db.execute("INSERT INTO assets (type, name, currency, symbol, yf_symbol, yf_name, url) VALUES (:type, :name, :currency, :symbol, :yf_symbol, :yf_name, :url)", {"type": type, "name": name, "currency": currency, "symbol": symbol, "yf_symbol": yf_symbol, "yf_name": yf_name, "url": url})
+            print(f"{count}. {name} Inserted")
+            count += 1
+    db.commit()
+
+insert_assets()
+db.close()
