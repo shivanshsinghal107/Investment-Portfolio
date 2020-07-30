7c69c65de97e2b5dc57eb0eeb131c792a1844568
Shivansh Singhal <shivanshsinghal107@gmail.com>
2020-07-30 13:15:07 +0530

    Update data.py

--- data.py
+++ data.py
@@ -79,3 +79,9 @@ def insert_assets():
 
     db.commit()
     db.close()
+
+make_tables()
+table_data = db.execute("SELECT * FROM assets").fetchall()
+db.close()
+if len(table_data) <= 0:
+    insert_assets()
